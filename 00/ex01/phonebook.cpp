b7f3adfd21424a133c88056b3d205e1bfc95965d
eunbi-bb <eunbicho1995@gmail.com>
2024-01-27 14:58:33 +0100

    Phonebook is working

--- 00/ex01/phonebook.cpp
+++ 00/ex01/phonebook.cpp
@@ -1,109 +1,260 @@
 #include <iostream>
 #include <string>
-
-class PhoneBook : public Contact
-{
-	private:
-		std::string contacts[8];
-		static const size_t	MAX_CONTACTS = 8;
-	public:
-		// Setter
-		void	setFirstName(std::string first);
-		void	setLastName(std::string last);
-		void	setNickName(std::string nick);
-		void	setPhoneNumber(std::string num);
-		void	setDarkSecret(std::string secret);
-		// Getter
-		void	getFirstName();
-		void	getLastName();
-		void	getNickName();
-		void	getPhoneNumber();
-		void	getDarkSecret();
-};
+#include <iomanip>
+#include "phonebook.hpp"
 
 class Contact
 {
-	public:
+	private:
 		std::string	firstName;
 		std::string	lastName;
 		std::string	nickName;
 		std::string	phoneNumber;
-		std::string	darkestSecret;
+		std::string	darkSecret;
+	public:
+		Contact() {} //Default constructor
+		Contact(std::string firstName, std::string lastName, std::string nickName, 
+				std::string phoneNumber, std::string darkSecret)
+		{
+			this->firstName = firstName;
+			this->lastName = lastName;
+			this->nickName = nickName;
+			this->phoneNumber = phoneNumber;
+			this->darkSecret = darkSecret;
+		}
+		std::string getFirstName();
+		std::string getLastName();
+		std::string	getNickName();
+		std::string	getPhoneNumber();
+		std::string	getDarkSecret();
 };
 
-//Setter funcs
-void	setFirstName(std::string first)
+class PhoneBook
 {
+	private:
+		Contact contacts[8];
+		int		index;
+	public:
+		PhoneBook() : index(0) {} //Initialize index in the constructor
+		void	printContactAllInfo(int index);
+		void	add(Contact contact);
+		void	search();
+		void	printIndex(int index);
+		void	printSummary();
+		
+		void	fillMaxContacts();
+};
 
+Contact	createContact(std::string firstName, std::string lastName, std::string nickName, 
+				std::string phoneNumber, std::string darkSecret)
+{
+	return Contact(firstName, lastName, nickName, phoneNumber, darkSecret);
 }
 
-void	setLastName(std::string last)
+void	PhoneBook::fillMaxContacts()
 {
-
+	std::string firstName0 = "Roy", 
+				lastName0 = "de Martines", 
+				nickName0 = "Royboy", 
+				phoneNumber0 = "0657678769", 
+				darkSecret0 = "He's a babo";
+	std::string firstName1 = "Eunbi", 
+				lastName1 = "Cho", 
+				nickName1 = "Zombie",
+				phoneNumber1 = "0638602453",
+				darkSecret1 = "She's cute";
+	std::string firstName2 = "Minie", 
+				lastName2 = "de Martines", 
+				nickName2 = "Dinosaurs", 
+				phoneNumber2 = "123456789", 
+				darkSecret2 = "She can bite hard.";
+	std::string firstName3 = "Yejin", 
+				lastName3 = "Park", 
+				nickName3 = "Bagyejin", 
+				phoneNumber3 = "01028089982", 
+				darkSecret3 = "She is the youngest";
+	std::string firstName4 = "Camila", 
+				lastName4 = "Ignacia Prado", 
+				nickName4 = "Kamille", 
+				phoneNumber4 = "987654321", 
+				darkSecret4 = "She has a daughter.";
+	std::string firstName5 = "Violeta", 
+				lastName5 = "Van Gennip Ignacia", 
+				nickName5 = "Viole", 
+				phoneNumber5 = "none", 
+				darkSecret5 = "She doesn't have any tooth.";
+	std::string firstName6 = "Bert", 
+				lastName6 = "Van Gennip", 
+				nickName6 = "Betty", 
+				phoneNumber6 = "147258369", 
+				darkSecret6 = "He's the father of Violeta";
+	std::string firstName7 = "Hyebin", 
+				lastName7 = "Park", 
+				nickName7 = "Bagabin", 
+				phoneNumber7 = "77777777", 
+				darkSecret7 = "She's stubborn.";
+	add(createContact(firstName0, lastName0, nickName0, phoneNumber0, darkSecret0));
+	add(createContact(firstName1, lastName1, nickName1, phoneNumber1, darkSecret1));
+	add(createContact(firstName2, lastName2, nickName2, phoneNumber2, darkSecret2));
+	add(createContact(firstName3, lastName3, nickName3, phoneNumber3, darkSecret3));
+	add(createContact(firstName4, lastName4, nickName4, phoneNumber4, darkSecret4));
+	add(createContact(firstName5, lastName5, nickName5, phoneNumber5, darkSecret5));
+	add(createContact(firstName6, lastName6, nickName6, phoneNumber6, darkSecret6));
+	add(createContact(firstName7, lastName7, nickName7, phoneNumber7, darkSecret7));
 }
 
-void	setNickName(std::string nick)
-{
 
+Contact	createFromInput()
+{
+	std::string firstName, lastName, nickName, phoneNumber, darkSecret;
+
+	std::cout << "\033[0;32mEnter first name : \n\033[0m >";
+	std::getline(std::cin, firstName);
+
+	std::cout << "\033[0;32mEnter last name : \n\033[0m >";
+	std::getline(std::cin, lastName);
+
+	std::cout << "\033[0;32mEnter nickname : \n\033[0m >";
+	std::getline(std::cin, nickName);
+
+	std::cout << "\033[0;32mEnter phone number : \n\033[0m >";
+	std::getline(std::cin, phoneNumber);
+
+	std::cout << "\033[0;32mEnter dark secret : \n\033[0m >";
+	std::getline(std::cin, darkSecret);
+
+	std::cout << "\033[1;32mA new contact is added. \n\n\033[0m";
+
+	return Contact(firstName, lastName, nickName, phoneNumber, darkSecret);
 }
 
-void	setPhoneNumber(std::string num)
+void	PhoneBook::add(Contact contact)
 {
-
+	this->contacts[index % 8] = contact;
+	this->index++;
 }
 
-void	setDarkSecret(std::string secret)
+void	err(const char *msg)
 {
+	std::cerr << msg << std::endl;
+	std::cin.clear();
+	std::cin.ignore(1000, '\n');
+}
 
+void	printShortenName(const std::string &str)
+{
+	if (str.length() > 10)
+		std::cout << std::setw(9) << std::setfill(' ') << str.substr(0, 9) << '.';
+	else
+		std::cout << std::setw(10) << str;
+}
+
+void	PhoneBook::printSummary()
+{
+	int	i = 0;
+
+	std::cout << std::endl; 
+	while (i < this->index)
+	{
+		std::cout << std::setw(10) << std::setfill(' ') << i;
+		std::cout << '|';
+		printShortenName(contacts[i].getFirstName());
+		std::cout << '|';
+		printShortenName(contacts[i].getLastName());
+		std::cout << '|';
+		printShortenName(contacts[i].getNickName());
+		std::cout << std::endl;
+		i++;
+	}
+}
+
+void	PhoneBook::printContactAllInfo(int index)
+{
+	std::cout << std::endl;
+	std::cout << "- First name		: " << contacts[index].getFirstName() << std::endl;
+	std::cout << "- Last name		: " << contacts[index].getLastName() << std::endl;
+	std::cout << "- Nickname		: " << contacts[index].getNickName() << std::endl;
+	std::cout << "- Phone number		: " << contacts[index].getPhoneNumber() << std::endl;
+	std::cout << "- Darkest secret	: " << contacts[index].getDarkSecret() << std::endl;
+	std::cout << std::endl;
+	std::cin.clear();
+	std::cin.ignore(1000, '\n');
+}
+
+void	PhoneBook::search()
+{
+	int	index;
+
+	if (this->index >= 1)
+	{
+		printSummary();
+		std::cout << "\033[0;32mType an index of a contact\n >\033[0m";
+		std::cin >> index;
+		if (index < 0 || index >= 8)
+		{
+			err("\e[38;5;208mTyped index is out of bound.\n >\033[0m");
+			return ;
+		}
+		else if (this->index < index)
+		{
+			err("\e[38;5;208mThe contact doesn't exist.\n >\033[0m");
+			return ;
+		}
+		else
+			printContactAllInfo(index);
+	}
+	else
+	{
+		std::cout << "\e[38;5;208mThe phonebook is empty.\n\n \033[0m";
+		return ;
+	}
 }
 
 //Getter funcs
-void	getFirstName(std::string first)
+std::string	Contact::getFirstName()
 {
-
+	return this->firstName;
 }
 
-void	getLastName(std::string last)
+std::string	Contact::getLastName()
 {
-
+	return this->lastName;
 }
 
-void	getNickName(std::string nick)
+std::string	Contact::getNickName()
 {
-
+	return this->nickName;
 }
 
-void	getPhoneNumber(std::string num)
+std::string	Contact::getPhoneNumber()
 {
-
+	return this->phoneNumber;
 }
 
-void	getDarkSecret(std::string secret)
+std::string	Contact::getDarkSecret()
 {
-
+	return this->darkSecret;
 }
 
-int	main(int argc, char **argv)
+int	main()
 {
 	PhoneBook	phoneBook;
-	Contact		contact;
+	std::string	cmd;
 
-	std::string	first, last, nick, num, secret;
-
-	std::cout << "Enter first name : ";
-	std::getline(std::cin, first);
-
-	std::cout << "Enter last name : ";
-	std::getline(std::cin, last);
-
-	std::cout << "Enter nickname : ";
-	std::getline(std::cin, first);
-
-	std::cout << "Enter phone number : ";
-	std::getline(std::cin, num);
-
-	std::cout << "Enter dark secret : ";
-	std::getline(std::cin, first);
-
+	while (true)
+	{
+		std::cout << "\033[0;32mPlease enter a command : (ADD, SEARCH or EXIT)\n\033[0m >";
+		std::getline(std::cin, cmd);
+		if (cmd == "TEST")
+			phoneBook.fillMaxContacts();
+		else if (cmd == "ADD")
+			phoneBook.add(createFromInput());
+		else if (cmd == "SEARCH")
+			phoneBook.search();
+		else if (cmd == "EXIT")
+			break ;
+		else
+			std::cout << "\e[38;5;208mInvalid command\n\033[0m"; 
+	}
 	return 0;
 }
---
+++ 00/ex01/phonebook.hpp
@@ -0,0 +1,8 @@
+#ifndef PHONEBOOK_H
+# define PHONEBOOK_H
+
+#define CYAN	\033[0;96m
+#define GREEN_B	\033[1;32m
+#define DEFAULT	\033[0m
+
+#endif
