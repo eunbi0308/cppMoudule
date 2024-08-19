c8aeb2a28b30964196c39b9b3255a156eaa2cc2a
eunbi-bb <eunbicho1995@gmail.com>
2024-08-19 17:53:31 +0200

    ex00 is done

---
+++ 08/ex00/easyfind.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+# define RED "\033[1;91m"
+# define GREY "\033[0;90m"
+# define BLUE "\033[38;5;38m"
+# define PURPLE	"\033[38;2;200;160;255m"
+# define DEFAULT "\033[0m"
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+typename T::const_iterator	easyfind(const T& container, int num)
+{
+	auto it = std::find(container.begin(), container.end(), num);
+	if (it != container.end())
+		return it;
+	return container.end();
+}
---
+++ 08/ex00/main.cpp
@@ -0,0 +1,61 @@
+#include "easyfind.hpp"
+
+int main()
+{
+	{
+		std::cout << BLUE << "\n_____ Basic test _____\n\n" << DEFAULT;
+		try
+		{
+			std::vector<int> vectorInt = {0, 3, 6, 9, 12, 15};
+			std::cout << PURPLE << "___ Elements in vector<int>\n" << DEFAULT;
+			for (auto it = vectorInt.begin(); it != vectorInt.end(); ++it)
+			{
+				std::cout << *it << " ";
+			}
+			std::cout << '\n';
+
+			std::cout << PURPLE << "___ Find '1' in vector<int>\n" << DEFAULT;
+			auto it = easyfind(vectorInt, 3);
+			if (it == vectorInt.end())
+				throw std::runtime_error("Value not found in the container.");
+			else
+				std::cout << "Found " << *it << '\n';
+			
+			std::cout << PURPLE << "___ Find '10' in vector<int>\n" << DEFAULT;
+			it = easyfind(vectorInt, 10);
+			if (it == vectorInt.end())
+				throw std::runtime_error("Value not found in the container.");
+			else
+				std::cout << "Found " << *it << '\n';
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << RED << e.what() << '\n' << DEFAULT;
+		}
+	}
+	{
+		std::cout << BLUE << "\n_____ First occurrence test _____\n\n" << DEFAULT;
+		try
+		{
+			std::vector<int> vectorInt = {0, 3, 6, 9, 9, 15};
+			std::cout << PURPLE << "___ Elements in vector<int>\n" << DEFAULT;
+			for (auto it = vectorInt.begin(); it != vectorInt.end(); ++it)
+			{
+				std::cout << *it << " ";
+			}
+			std::cout << '\n';
+
+			std::cout << PURPLE << "___ Check the first '9' occurrence\n" << DEFAULT;
+			auto it = easyfind(vectorInt, 9);
+			if (it != vectorInt.begin() + 3)
+				throw std::runtime_error("Incorrect first occurrence of value.");
+			else
+				std::cout << "Found " << *it << '\n';
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << RED << e.what() << '\n' << DEFAULT;
+		}
+	}
+	return 0;
+}
